80590073e6e07ccbe748d6a88dcc233d173a790d
938938 <938kjh@gmail.com>
2025-09-23 22:11:00 +0900

    [STYLE] 색상 적용

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,31 @@
 export const lightTheme = {
   background: '#FFFFFF',
-  text: '#000000',
+  systemText: '#000000',
+  text: '#080808',
+  magnolia: '#eae4e9ff',
+  linen: '#fff1e6ff',
+  mistyRose: '#fde2e4ff',
+  mimiPink: '#fad2e1ff',
+  mintCream: '#e2ece9ff',
+  lightBlue: '#bee1e6ff',
+  isabelline: '#f0efebff',
+  lavenderWeb: '#dfe7fdff',
+  periwinkle: '#cddafdff',
 };
 
 export const darkTheme = {
   background: '#2B2B30',
-  text: '#FFFFFF',
+  systemText: '#FFFFFF',
+  text: '#080808',
+  magnolia: '#eae4e9ff',
+  linen: '#fff1e6ff',
+  mistyRose: '#fde2e4ff',
+  mimiPink: '#fad2e1ff',
+  mintCream: '#e2ece9ff',
+  lightBlue: '#bee1e6ff',
+  isabelline: '#f0efebff',
+  lavenderWeb: '#dfe7fdff',
+  periwinkle: '#cddafdff',
 };
 
 export type ThemeType = typeof lightTheme;
