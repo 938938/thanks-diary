888d229bbb514355d28fa2a7254173c2fb8ab55d
938938 <938kjh@gmail.com>
2025-09-23 21:19:16 +0900

    [FEAT] react-query 세팅

--- src/config/clientLayout.tsx
+++ src/config/clientLayout.tsx
@@ -1,13 +1,17 @@
 'use client';
 import { darkTheme, lightTheme } from '@/styles/theme';
 import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
+
+export const queryClient = new QueryClient({});
 
 export default function ClientLayout({ children }: { children: ReactNode }) {
   const isDark = false;
+
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ThemeProvider>
   );
 }
