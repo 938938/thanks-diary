bf0aae6b2245abbccd57e4230dc422e0dbdf2092
938938 <938kjh@gmail.com>
2025-09-23 22:21:53 +0900

    [STYLE] 폰트 적용

---
+++ src/styles/fonts.ts
@@ -0,0 +1,29 @@
+import {
+  Nanum_Brush_Script,
+  Nanum_Pen_Script,
+  Gamja_Flower,
+  Gaegu,
+} from 'next/font/google';
+
+export const nanumBrushScript = Nanum_Brush_Script({
+  weight: ['400'],
+});
+
+export const nanumPenScript = Nanum_Pen_Script({
+  weight: ['400'],
+});
+
+export const gamjaFlower = Gamja_Flower({
+  weight: ['400'],
+});
+
+export const gaegu = Gaegu({
+  weight: ['400'],
+});
+
+export const fonts = {
+  nanumBrushScript,
+  nanumPenScript,
+  gamjaFlower,
+  gaegu,
+};
