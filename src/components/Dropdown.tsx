b6f584d9e3755c1631345b60080b6e3f8defb2f3
938938 <938kjh@gmail.com>
2025-10-01 22:03:56 +0900

    [FEAT] 드롭다운 적용

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { Dispatch, useState } from 'react';
+import styled from 'styled-components';
+
+const Component = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+const SelectButton = styled.button`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  height: 2rem;
+`;
+
+const Select = styled.div`
+  outline: none;
+  border: none;
+  text-align: left;
+`;
+
+const DropDown = styled.div`
+  z-index: 99;
+  position: absolute;
+  top: 2rem;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  @keyframes dropdown {
+    0% {
+      transform: translateY(-5%);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+  animation: dropdown 0.4s ease;
+`;
+
+const Option = styled.button`
+  height: 2rem;
+  border: 0;
+  border-radius: 5px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Dropdown = ({
+  data,
+  setSelected,
+  selected,
+}: {
+  data: string[];
+  selected: string;
+  setSelected: Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [isDropDown, setIsDropDown] = useState<boolean>(false);
+
+  const onClickOptionHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelected(e.currentTarget.innerText);
+    setIsDropDown(false);
+  };
+
+  const onClickSelect = () => {
+    setIsDropDown(!isDropDown);
+  };
+  return (
+    <Component>
+      <SelectButton type='button' onClick={onClickSelect}>
+        <Select>{selected}</Select>
+      </SelectButton>
+      {isDropDown && (
+        <DropDown>
+          {data.map((item) => (
+            <Option key={item} onClick={onClickOptionHandler}>
+              {item}
+            </Option>
+          ))}
+        </DropDown>
+      )}
+    </Component>
+  );
+};
+
+export default Dropdown;
--- src/components/InputComponents.tsx
+++ src/components/InputComponents.tsx
@@ -1,14 +1,42 @@
 'use client';
 
 import styled from 'styled-components';
+import Dropdown from './Dropdown';
+import { useState } from 'react';
+import useCreateItem from '@/hooks/useCreateItem';
 
-const Textarea = styled.textarea``;
+const InputBox = styled.div`
+  padding: 10px;
+`;
+const Textarea = styled.textarea`
+  width: 100%;
+`;
+const ConfirmBtn = styled.button``;
 
 const InputComponents = () => {
+  const { mutate } = useCreateItem();
+  const [text, setText] = useState<string>('');
+  const [color, setColor] = useState<string>('');
+  const [pin, setPin] = useState<string>('');
+  const [font, setFont] = useState<string>('');
+  const onClickHandler = () => {
+    mutate({
+      text,
+      color,
+      pin,
+      font,
+    });
+  };
   return (
-    <div>
-      <Textarea />
-    </div>
+    <InputBox>
+      <Dropdown data={['red', 'blue', 'black', 'green']} selected={color} setSelected={setColor} />
+      <Textarea
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <ConfirmBtn onClick={onClickHandler}>완료!</ConfirmBtn>
+    </InputBox>
   );
 };
 
